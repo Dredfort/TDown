2d40f76a30051c497b4b59cd011167e8331db433
Oleg <oleg.fen@gmail.com>
2016-03-22 10:53:39 +0200

    test commit

--- Source/TDown/Heatmap/Heatmap.h
+++ Source/TDown/Heatmap/Heatmap.h
@@ -7,6 +7,13 @@
 
 class ATDownCharacter;
 
+UENUM(BlueprintType)
+enum class ESplineDataSwitcher : uint8
+{
+	ES_ArrayData UMETA(DisplayName ="ArrayData"),
+	ES_StringData		UMETA(DisplayName = "StringData"),
+};
+
 UCLASS()
 class TDOWN_API AHeatmap : public AActor
 {
@@ -15,35 +22,52 @@ class TDOWN_API AHeatmap : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AHeatmap(const FObjectInitializer& ObjectInitializer);
-
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	
+	virtual void BeginPlay() override;	
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
 	/*_________________________________________________*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
+		ESplineDataSwitcher SplineDataSwitcher;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_")
+	UBillboardComponent* Billboard;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
 	class USplineComponent* PathSpline;
-	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
 		float TimeBetweenChek = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
+		bool bBuildSpline = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
+		bool bCollectData = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
+		bool bRewritetData = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
+		uint8 CharPtrNumber = 0;
+	UFUNCTION(BlueprintCallable, Category = "_HeatMap")
+		void CheckCharacters();
 
 	UFUNCTION(BlueprintCallable, Category = "_HeatMap")
-	void CheckCharacters();
-	FTimerHandle ChekTimeHandler;
-
-
-	UWorld* cWorld;
-	ATDownCharacter* CharPtr;
-	TArray<ATDownCharacter*> CharactersArr;
-
-
+		void BuildSplinePath();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
 	FString SaveDirectoryPath = "E:/UE4_Projects/TDown/T";
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_HeatMap")
 	FString LogFileName = "LocationLogs.txt";
-	bool VerifyOrCreateDirectory(const FString& SaveDir, const FString& FileName, const FString& StringToSave) const;
 
-	bool SaveArrayToFile(const TArray<int32>& Array, const TCHAR* Filename, IFileManager * FileManager = &IFileManager::Get(), uint32 WriteFlags = 0) const;
-	bool LoadFileToArray(TArray<int32>& Result, const TCHAR* Filename, uint32 Flags);
+	FString StringOfCoords;
+	int64 it;
+	bool VerifyOrCreateDirectory( FString& SaveDir,  FString& FileName,  FString& StringToSave) ;
+
+	bool SaveArrayToFile(const TArray<int16>& Array, const TCHAR* Filename, IFileManager * FileManager = &IFileManager::Get(), uint32 WriteFlags = 0) const;
+	bool LoadFileToArray(TArray<int16>& Result, const TCHAR* Filename, uint32 Flags);
+	TArray<int16> ArrayFromFile;
+
+	
+
+private:
+	TArray<ATDownCharacter*> CharactersArr;
+	UWorld* cWorld;
+	FTimerHandle ChekTimeHandler;
+	ATDownCharacter* CharPtr;
 };
